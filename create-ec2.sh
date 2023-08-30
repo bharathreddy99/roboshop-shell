1c19d6a0e2d9b09f64cce909804f019462ae576e
Bharath <kbharathreddy99@gmail.com>
2023-08-30 21:15:37 +0530

    first

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,7 +11,6 @@ DOMAIN_NAME=learnndevops.online
 for i in "${NAMES[@]}"
 do  
    INSTANCE_TYPE="t2.micro"
-    fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
