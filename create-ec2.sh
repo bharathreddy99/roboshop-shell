f3747023caea962babbec5874674fad61cd3c2e8
Bharath <kbharathreddy99@gmail.com>
2023-08-30 21:14:23 +0530

    first

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,24 +3,20 @@
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0189d4870cc65fd55
-DOMAIN_NAME=joindevops.online
+SECURITY_GROUP_ID=sg-059a5b2fa98262417
+DOMAIN_NAME=learnndevops.online
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
 for i in "${NAMES[@]}"
 do  
-    if [[ $i == "mongodb" || $i == "mysql" ]]
-    then
-        INSTANCE_TYPE="t3.medium"
-    else
-        INSTANCE_TYPE="t2.micro"
+   INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z098285010FMU6PDV8O9P --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id Z0533349KCLA3APNMIVZ --change-batch '
     {
             "Changes": [{
             "Action": "CREATE",
